5c9fa04a24bae1853b41809a0b46203f180f40d8
Mark Essel <messel@gmail.com>
2012-07-27 12:48:29 -0400

    removed gif support at the moment to see if I can get cameo-bobross to build

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-npm install Nile-Delta
+#npm install Nile-Delta
+wget -O ./src/logStream.h https://raw.github.com/victusfate/Nile-Delta/master/src/logStream.h
 
+node-gyp rebuild
+
+exit 0
