fe180f97e1c6a780b198871e9399c4f32ef0b575
Mark Essel <messel@gmail.com>
2012-10-01 14:46:06 -0400

    lowered jobs to 6

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@ npm install Nile-Delta
 #wget -O ./src/logStream.h https://raw.github.com/victusfate/Nile-Delta/master/src/logStream.h
 
 node-gyp clean
-`node-gyp configure` make --jobs 10 -C build
+`node-gyp configure` make --jobs 6 -C build
 
 exit 0
